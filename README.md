647b7621af678392cfe0510fadb9b30e1b7be352
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2017-11-28 21:36:56 +0100

    Initialisation du projet

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TopQuiz
 Jeu où il faut répondre aux questions posées sous forme de QCM.
+
+[OpenClassrooms — Développez votre première application Android](https://openclassrooms.com/courses/developpez-votre-premiere-application-android)
---
+++ app/src/main/java/fr/fcomte/univ/iut/martin/florent/topquiz/MainActivity.java
@@ -0,0 +1,15 @@
+package fr.fcomte.univ.iut.martin.florent.topquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import static fr.fcomte.univ.iut.martin.florent.topquiz.R.layout.activity_main;
+
+public final class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main);
+    }
+}
